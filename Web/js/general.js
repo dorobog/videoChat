2a066e3e393d339fa292d7b28adaeeaff4c4d846
Joshua Akande Interns <jaakande@sidmach.com>
2018-06-14 08:14:35 +0100

    Created function for users to make a call by creating a session added new file to get root url for both api and web client

---
+++ Web/js/general.js
@@ -0,0 +1,21 @@
+﻿var clientBaseUrl = getRootUrl();
+var check = clientBaseUrl === "http://localhost:58163/"
+var apiBaseUrl = check ? "http://localhost:52978/" : "http://localhost:52978/";
+
+//generate guid
+var USERID = window.localStorage.getItem("userID");
+function guid() {
+    var gd = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = Math.random() * 16 | 0,
+			v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+    return gd;
+}
+function getRootUrl() {
+	//debugger;
+	var l = window.location.origin
+		? window.location.origin + '/'
+		: window.location.protocol + '/' + window.location.host + '/'; 
+  return l;
+}
