a8485130725249df4c1e23ed2ccf2f5a32c5163b
Joshua Akande Interns <jaakande@sidmach.com>
2018-06-20 03:43:37 +0100

    created a web worker to use xmlhttp request

---
+++ Web/js/checkcall.js
@@ -0,0 +1,18 @@
+﻿function receiveCall(obj) {
+	var workerResult = "";
+	if (typeof obj === "object") {
+		console.log(obj);
+		console.log('Message received from main script');
+		var workerResult = obj;
+		console.log('Posting message back to main script');
+	}
+	else {
+		setTimeout("receiving()", 500);
+	}
+	postMessage(workerResult);
+}
+
+function receiving() {
+	ajaxcall(apiBaseUrl + "api/IsUserAvailable/" + USERID, "", "GET", "json", receiveCall)
+}
+receiving();
